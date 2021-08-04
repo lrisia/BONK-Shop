a579c8581e4511a0824d3bb127680a56bbdb8cc7
Irisia <pochara.su@ku.th>
2021-08-04 11:05:08 +0700

    Add login, register page and login controller

--- src/main/java/ku/cs/App.java
+++ src/main/java/ku/cs/App.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -17,7 +18,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("login_page"), 600, 400);
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/ku/cs/controllers/LoginPageController.java
@@ -0,0 +1,42 @@
+package ku.cs.controllers;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginPageController {
+
+    @FXML private Button loginBtn;
+    @FXML private Button switchToRegisterBtn;
+    @FXML private TextField inputUsernameTextField;
+    @FXML private PasswordField inputPasswordField;
+    @FXML private Label closeLabel;
+    @FXML private Label loginMessageLabel; //Username หรือ Password ไม่ถูกต้อง! กรุณาลองใหม่
+    @FXML private ImageView myCatImageView;
+
+
+
+    @FXML
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File myCatFile = new File("images/พาแมวมาทะเล.jpeg");
+        Image myCatImage = new Image(myCatFile.toURI().toString());
+        myCatImageView.setImage(myCatImage);
+    }
+
+    @FXML
+    public void clickCloseLabel(Event event) {
+        Stage stage = (Stage) closeLabel.getScene().getWindow();
+        stage.close();
+    }
+}
