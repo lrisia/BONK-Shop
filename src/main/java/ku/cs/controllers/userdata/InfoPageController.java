8cd2f8a15c7110705e13282a1a17bbe3041822e0
Muad Anawat <Anawat.h@ku.th>
2021-09-09 15:26:33 +0700

    linked

---
+++ src/main/java/ku/cs/controllers/userdata/InfoPageController.java
@@ -0,0 +1,16 @@
+package ku.cs.controllers.shop;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+import java.io.IOException;
+
+public class InfoPageController {
+    @FXML ImageView logoImageView;
+    @FXML ImageView profileImageView1;
+    @FXML ImageView profileImageView2;
+    @FXML ImageView profileImageView3;
+
+
+
+}
